11823230fcd37ed3cb87145a4622cdd620c92fb6
Luke Woodward <luke.wdwrd@gmail.com>
2017-01-12 11:50:31 -0600

    authenticates user register and log-in

--- app.js
+++ app.js
@@ -7,24 +7,76 @@ firebase.initializeApp({
     databaseURL: "https://practice-authentication.firebaseio.com",
     storageBucket: "practice-authentication.appspot.com",
     messagingSenderId: "1070424131531"
-  });
+});
 
 
 
-$('.login-button').click((e) => {
+//works on load and on sign-out
+firebase.auth().onAuthStateChanged(() => {
+    setTimeout(() =>  console.log(firebase.auth().currentUser), 1000);
+    // CHECK LOGIN STATUS
+    if (firebase.auth().currentUser !== null) {
+        $('.login-page').addClass('hide');
+        $('.main-page').removeClass('hide');
+
+    } else {
+        $('.login-page').removeClass('hide');
+        $('.main-page').addClass('hide');
+    }
+
+})
+
+
+$('form').submit((e) => {
+
     var email = $('input[type="email"]').val()
     var password = $('input[type="password"]').val()
-    //set to h1
-    $('.main-page h1').text(`Welcome ${email}`)
-    //switch views
-    $('.login-page').addClass('hide');
-    $('.main-page').removeClass('hide');
+    firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password)
+        .then(() => {
+            //set to h1
+            $('.main-page h1').text(`Welcome ${email}`)
+            //switch views
+            $('.login-page').addClass('hide');
+            $('.main-page').removeClass('hide');
 
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
+        }).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(errorCode, errorMessage);
         // ...
-    });
+        })
     e.preventDefault();
+})
+
+
+
+$('.btn-warning').click((e) => {
+
+    var email = $('input[type="email"]').val()
+    var password = $('input[type="password"]').val()
+    firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then(() => {
+            //set to h1
+            $('.main-page h1').text(`Welcome ${email}`)
+            //switch views
+            $('.login-page').addClass('hide');
+            $('.main-page').removeClass('hide');
+
+        }).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(errorCode, errorMessage);
+        // ...
+        })
+    e.preventDefault();
+})
+
+$('.signOut').click(() => {
+    firebase.auth().signOut();
 })
